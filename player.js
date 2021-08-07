ea6dbd2b493f440a4af6a60e051a034987650640
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2021-08-07 17:23:16 -0400

    Add player class and game class

---
+++ player.js
@@ -0,0 +1,19 @@
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.wins = 0;
+    this.fighter = "";
+  }
+
+  savePlayerToStorage() {
+    var strPlayer = JSON.stringify(this);
+    localStorage.setItem(this.name, strPlayer);
+  }
+
+  getWinsFromStorage() {
+    var playerObj = JSON.parse(localStorage.getItem(this.name));
+    return playerObj.wins;
+  }
+
+  takeTurn() {}
+}
